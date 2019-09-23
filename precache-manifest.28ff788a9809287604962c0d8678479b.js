237872a49cf1f820844789efc21e65cb90a5f028
numeroflip <berenyi.aron@hotmail.hu>
2019-09-23 13:56:04 +0200

    Updates

---
+++ precache-manifest.28ff788a9809287604962c0d8678479b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62e56e026b8a49373cd848c1bbafc49",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "51202b763e4ed5efa539",
+    "url": "/monsters-rolodex/static/css/main.893a653f.chunk.css"
+  },
+  {
+    "revision": "842745e26725eb7f74a7",
+    "url": "/monsters-rolodex/static/js/2.9a4ddc69.chunk.js"
+  },
+  {
+    "revision": "51202b763e4ed5efa539",
+    "url": "/monsters-rolodex/static/js/main.0bd2f1ad.chunk.js"
+  },
+  {
+    "revision": "8e13eadf9c8cb6302839",
+    "url": "/monsters-rolodex/static/js/runtime~main.08a80c95.js"
+  }
+]);
